cd39672aa853be812e3d4cf142285f49efda8d0d
progand <lach@ukr.net>
2015-11-25 09:28:31 +0200

    [*] file uploading experiments

--- app/components/cave-element-edit-form.js
+++ app/components/cave-element-edit-form.js
@@ -3,8 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   item: null,
   actions: {
-    save() {
-      this.item.save();
+    changeImage() {
+      let $form = $('#cave-element-edit-form'),
+        data = $form.serialize();
+
     }
   }
 });

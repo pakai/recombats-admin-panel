d8a6ec5c5211c2c69eef33b5b70574173dfe9036
progand <lach@ukr.net>
2015-12-03 19:36:08 +0200

    [*] Admin panel. Create bot/artifact
    error message on save

--- app/models/bot.js
+++ app/models/bot.js
@@ -3,13 +3,13 @@ import CaveModelMixin from '../mixins/cave-model';
 
 export default DS.Model.extend(CaveModelMixin, {
   level: DS.attr('number'),
-  map: DS.attr('string'),
-  align: DS.attr('number'),
+  map: DS.attr('string', { defaultValue: '' }),
+  align: DS.attr('number', { defaultValue: 0 }),
   hp: DS.attr('number'),
-  stats: DS.attr('string'),
-  adds: DS.attr('string'),
-  priems: DS.attr('string'),
-  sex: DS.attr('number'),
-  clan: DS.attr('number'),
-  izlom: DS.attr('number')
+  stats: DS.attr('string', { defaultValue: '' }),
+  adds: DS.attr('string', { defaultValue: '' }),
+  priems: DS.attr('string', { defaultValue: '' }),
+  sex: DS.attr('number', { defaultValue: 0 }),
+  clan: DS.attr('number', { defaultValue: 0 }),
+  izlom: DS.attr('number', { defaultValue: 0 })
 });
